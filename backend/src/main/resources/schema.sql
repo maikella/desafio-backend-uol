68fda6356fe12848dea936185e528870780d9c7c
maikella <97262283+maikella@users.noreply.github.com>
2025-09-18 14:02:30 -0300

    chore: add database config

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE jogadores (
+    codinome VARCHAR(20) NOT NULL,
+    nome VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    telefone VARCHAR(11),
+    grupo VARCHAR(15) NOT NULL
+);
